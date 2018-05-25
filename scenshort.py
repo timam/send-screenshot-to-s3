1762ed0aace79f070c6e1653ba29f0f1f864e6e3
Timam <me@timam.io>
2018-05-26 02:43:29 +0600

    Taking screenshot and sending to s3

---
+++ scenshort.py
@@ -0,0 +1,32 @@
+#-- include('examples/showgrabfullscreen.py') --#
+import pyscreenshot as ImageGrab
+import time
+import boto3
+
+from secrets import *
+
+
+if __name__ == "__main__":
+    # fullscreen
+    im=ImageGrab.grab()
+    timestr = time.strftime("%Y%m%d-%H%M%S")
+    ImageGrab.grab_to_file(timestr +'my.png')
+    image_name = timestr +'my.png'
+
+    data = open(image_name, 'rb')
+
+    s3 = boto3.resource(
+        's3',
+        aws_access_key_id=gimme_aws_access_id(),
+        aws_secret_access_key=gimme_aws_secret_access_key()
+    )
+    s3.Bucket('screenshot-python').put_object(Key=image_name, Body=data)
+
+    print ("Done")
+    sleep(20)
+
+
+    
+    
+    
+    
