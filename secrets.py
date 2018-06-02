ce17efa7182a4da6768a4c7ff76d7668355fb783
Timam <me@timam.io>
2018-06-02 15:28:10 +0600

    fresh secrets module added

---
+++ secrets.py
@@ -0,0 +1,12 @@
+def gimme_aws_region():
+    return '-------------'
+
+def gimme_aws_access_id():
+    return '---------------------------'
+
+def gimme_aws_secret_access_key():
+    return '----------------------------------------'
+
+
+
+
